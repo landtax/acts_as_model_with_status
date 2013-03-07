13a163c75c7de098c1b8f29e22ed1720b0dcfa76
Santi <santiago.bel@webfordesigners.com>
2013-03-07 18:29:57 +0100

    cleaning

--- lib/acts_as_model_with_status/railtie.rb
+++ lib/acts_as_model_with_status/railtie.rb
@@ -2,13 +2,10 @@ require 'rails'
 require 'active_record'
 require 'acts_as_model_with_status'
 
-p "Railtie loaded"
-
 begin
   module ActsAsModelWithStatus
     class Railtie < Rails::Railtie
       config.to_prepare do
-        p "hook added"
         ActiveRecord::Base.send(:extend, ActsAsModelWithStatus::Hook)
       end
     end
